2fc585fcb9ef4bbaa77ed5db28c3dd3e645a9b9b
Samuel Milhomens <samuel.mil.mar@gmail.com>
2022-08-12 16:25:05 -0300

    Completed first version home

--- src/views/home.php
+++ src/views/home.php
@@ -2,8 +2,47 @@
     /**
      * @var App\Core\View $this
      */
-     $this->setStyleSheet('style');
+     $this->setStyleSheet('home');
      $this->subTitle = 'Home';
 ?>
 
-<h3><?= $nome ?></h3>
+<section class="about">
+    <img class="user_image" src="https://github.com/samuel-mil.png" alt="name">
+    <div class="user_infos">
+        <h1>Samuel Milhomens</h1>
+        <strong>Programador | CEO</strong>
+    </div>
+    <div class="user_description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ad dolores dignissimos voluptates reiciendis magni quo inventore cumque saepe qui est, totam libero. Tenetur excepturi ipsa, nihil pariatur aspernatur corporis.</p>
+    </div>
+</section>
+
+<section class="posts_container">
+    <?php for($i = 0; $i < 5; $i++): ?>
+    <div class="post_single">
+        <h1 class="post_title">
+            <a href="#">Como aprender a sua primeira linguagem de programação</a>
+        </h1>
+
+        <div class="post_infos">
+            <div class="post_date">
+                <i class="fa-solid fa-calendar-days"></i>
+                <span>10/04/22</span>
+            </div>
+
+            <div class="post_tag">
+                <i class="fa-solid fa-tag"></i>
+                <span>Programação</span>
+            </div>
+        </div><!--post_infos-->
+
+        <img class="post_image" src="https://www.aulasdejapones.com.br/wp-content/uploads/2020/04/Blog-Japon%C3%AAs-do-Dia-a-Dia-9-600x315.png" alt="background">
+
+        <div class="post_content_preview">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod numquam magni possimus expedita iure nisi nesciunt, rem dolorem quis repellendus quam laudantium iste esse error sed? Natus impedit at pariatur?</p>
+        </div>
+
+        <a class="post_btn" href="#">Ler mais</a>
+    </div>
+    <?php endfor; ?>
+</section>
--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -2,13 +2,29 @@
 <html lang="pt-br">
 <head>
     <title>Blog | <?= $this->subTitle ?></title>
+    <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="<?= $this->getStyleSheet() ?>">
+    <script defer src="https://kit.fontawesome.com/3fb918250b.js" crossorigin="anonymous"></script>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
 </head>
 <body>
-    <h3>Hello layout</h3>
-    {{content}}
+    <header>
+        <div class="container">
+            <h1>Samuel's Blog</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About me</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <div id="content">
+        {{content}}
+    </div>
 </body>
 </html>
